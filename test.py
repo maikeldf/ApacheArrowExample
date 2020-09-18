e9acb71b1cc390ff20071fd143f55b078adeb0ae
maikeldf <maikeldf@gmail.com>
2020-09-18 12:17:36 +0100

    add source

---
+++ test.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import pyspark
+import time
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("PyArrow_Test").enableHiveSupport().getOrCreate()
+
+# Creating two different pandas DataFrame with same data
+pdf1 = pd.DataFrame(np.random.rand(100000, 3))
+pdf2 = pd.DataFrame(np.random.rand(100000, 3))
+
+start = time.time()
+spark.createDataFrame(pdf1)
+end = time.time()
+print("Conversion time of Pandas DataFrames to Spark DataFrames:", end - start)
+
+# Using PyArrow in Spark to optimize the above Conversion
+
+# Enable Arrow-based columnar data transfers in Spark
+spark.conf.set("spark.sql.execution.arrow.enabled", "true")
+
+start = time.time()
+df2 = spark.createDataFrame(pdf2)
+end = time.time()
+print("Conversion time of Pandas DataFrames to Spark DataFrames using PyArrow:", end - start)
